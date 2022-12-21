4e37470fbba0607b4a854b9e77d627017bef4229
EJ Fox <ejfox@ejfox.com>
2022-12-21 17:28:22 -0500

    refactor: update ssr to false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
   css: ["tachyons/css/tachyons.min.css"],
   modules: ['@nuxt/content'],
   content: {
